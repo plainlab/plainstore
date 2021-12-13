29f840b12459f1726eb5b1dd3208eb157d7449ec
manhtai <vo@manhtai.com>
2021-12-13 22:24:05 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,16 @@
+PlainStore
+==========
+
+> Dead simple store for digital goods
+
+
+## Local setup
+
+```sh
+poetry install
+poetry run src/manage.py migrate
+poetry run src/manage.py createsuperuser
+poetry run src/manage.py runserver
+```
+
+Enjoy!
---
+++ src/plainstore/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0kk%0-md3(1(-h%&#0ww1wur=im)8m6mi$x#1(%ohxc@8@5w$n'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
